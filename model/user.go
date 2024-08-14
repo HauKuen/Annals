07ec249ce5734d94746e8894c3df3d85b4688615
haukuen <haukuen@foxmail.com>
2024-08-14 17:36:40 +0800

    feat(user): 使用bcrypt加密用户密码

--- model/user.go
+++ model/user.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"fmt"
 	"github.com/HauKuen/Annals/utils/errmsg"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -19,10 +21,26 @@ type APIUser struct {
 	CreatedAt string `json:"created_at"`
 }
 
+// BeforeCreate GORM 的钩子，在创建用户前加密密码
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %w", err)
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+// VerifyPassword 验证密码
+func (u *User) VerifyPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
+
 // GetUser 查询用户
 func GetUser(id int) (User, int) {
 	var user User
-	err = db.Limit(1).Where("ID = ?", id).Find(&user).Error
+	err := db.Limit(1).Where("ID = ?", id).Find(&user).Error
 	if err != nil {
 		return user, errmsg.ERROR
 	}
@@ -40,8 +58,8 @@ func GetUsers(pageSize int, pageNum int) ([]APIUser, int64) {
 }
 
 // CreateUser 添加用户
-func CreateUser(data *User) (code int) {
-	err = db.Create(&data).Error
+func CreateUser(data *User) int {
+	err := db.Create(data).Error
 	if err != nil {
 		return errmsg.ERROR // 500
 	}
@@ -49,7 +67,7 @@ func CreateUser(data *User) (code int) {
 }
 
 // CheckUser 查询用户是否存在
-func CheckUser(username string) (code int) {
+func CheckUser(username string) int {
 	var user User
 	db.Select("id").Where("username = ?", username).First(&user)
 	if user.ID > 0 {
